f180472aff005c5880fa1b783a339b8da7374f7a
Vini <viniciuscirne7@gmail.com>
2025-08-26 15:24:45 -0300

    Timesdefutebol

---
+++ times.js
@@ -0,0 +1,6 @@
+let timesDeFutebol = ["Santa Cruz", "Sport", "Nautico", "Retro"];
+// imprimir nome do primeiro time 
+{console.log(timesDeFutebol[0]);
+// imprimir nome do ultimo time 
+console.log(timesDeFutebol[3]); 
+}
